3cd8f31b4e8442de4e0d87e12559e51ccbb17605
Juan Orlando <jcorlandomc@yahoo.com>
2021-01-06 17:20:39 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+import time
+import shutil
+
+response = requests.get("http://oasis.caiso.com/oasisapi/SingleZip?queryname=SLD_FCST&market_run_id=DAM&startdatetime=20160415T07:00-0000&enddatetime=20160416T07:00-0000&version=1")
+
+
+print('')
+print('The response code is:', response.status_code)
+
+
+
+
+print('\nThis is the end of the program')
+
