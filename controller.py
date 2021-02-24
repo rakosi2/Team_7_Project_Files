a0aa5d6f10222d0af2a5c3fc20e4a4fa884ec8f0
Juan Carlos Orlando <jcorlandomc@yahoo.com>
2021-02-24 02:24:32 -0800

    02/24/2021 02:23 am

--- controller.py
+++ controller.py
@@ -5,16 +5,23 @@ import time
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 GPIO.setup(29,GPIO.OUT)
+GPIO.setup(31,GPIO.OUT)
+GPIO.setup(37,GPIO.OUT)
+GPIO.setup(36,GPIO.OUT)
+
 
 while True:
     GPIO.output(29,GPIO.HIGH)
-    time.sleep(1)
+    time.sleep(0.01)
     GPIO.output(29,GPIO.LOW)
-    time.sleep(1)
-
-
-
-
-
-
-
+    GPIO.output(31,GPIO.HIGH)
+    time.sleep(0.01)
+    GPIO.output(31,GPIO.LOW)
+    GPIO.output(37,GPIO.HIGH)
+    time.sleep(0.01)
+    GPIO.output(37,GPIO.LOW)
+    GPIO.output(36,GPIO.HIGH)
+    time.sleep(0.01)
+    GPIO.output(36,GPIO.LOW)
+    
+    
