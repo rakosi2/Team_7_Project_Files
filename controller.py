deb97e820945c096377f98556f8f5f80a07e088e
Juan Carlos Orlando <jcorlandomc@yahoo.com>
2021-02-26 15:56:16 -0800

    02/26/2021 03:55 pm

--- controller.py
+++ controller.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 import RPi.GPIO as GPIO
 import time
+import pigpio
+
+pi = pigpio.pi()
+pi.hardware_PWM(12, 800, 10000)   # <---- Research this more to understand better
+
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
@@ -9,19 +14,17 @@ GPIO.setup(31,GPIO.OUT)
 GPIO.setup(37,GPIO.OUT)
 GPIO.setup(36,GPIO.OUT)
 
-
 while True:
     GPIO.output(29,GPIO.HIGH)
-    time.sleep(0.01)
+    time.sleep(0.10)
     GPIO.output(29,GPIO.LOW)
     GPIO.output(31,GPIO.HIGH)
-    time.sleep(0.01)
+    time.sleep(0.10)
     GPIO.output(31,GPIO.LOW)
     GPIO.output(37,GPIO.HIGH)
-    time.sleep(0.01)
+    time.sleep(0.10)
     GPIO.output(37,GPIO.LOW)
     GPIO.output(36,GPIO.HIGH)
-    time.sleep(0.01)
+    time.sleep(0.10)
     GPIO.output(36,GPIO.LOW)
-    
     
