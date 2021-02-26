350daa1e0acb342eb8deb91026f958abb976417a
Juan Carlos Orlando <jcorlandomc@yahoo.com>
2021-02-26 14:31:11 -0800

    02/26/2021 02:30 pm

--- caiso_api.py
+++ caiso_api.py
@@ -6,16 +6,21 @@ import json
 import datetime
 
 todays_date = datetime.datetime.now()
+yesterdays_date = todays_date - datetime.timedelta(days = 1)
+tomorrows_date = todays_date + datetime.timedelta(days = 1)
 string_tail = 'T07:00-0000'
 
-print("1st url string: ", todays_date.strftime("%Y") + todays_date.strftime("%m") + todays_date.strftime("%d") + string_tail)
-print("2nd url string: ", todays_date.strftime("%Y") + todays_date.strftime("%m") + todays_date.strftime("%d") + string_tail)
+first_url_string = todays_date.strftime("%Y") + todays_date.strftime("%m") + todays_date.strftime("%d") + string_tail
+second_url_string = tomorrows_date.strftime("%Y") + tomorrows_date.strftime("%m") + tomorrows_date.strftime("%d") + string_tail
+
+print("1st url string: ", first_url_string)
+print("2nd url string: ", second_url_string)
 
 
 response = requests.get("http://oasis.caiso.com/oasisapi/SingleZip?queryname=SLD_FCST&market_run_id=DAM&startdatetime=20160415T07:00-0000&enddatetime=20160416T07:00-0000&version=1")
 
 
-print('\nThe response code is:', response.status_code)
+print('The response code is:', response.status_code)
 
 
 
