9b5699007f9eccb09b98588b5ca7a2d9433cf2dc
Juan Carlos Orlando <jcorlandomc@yahoo.com>
2021-03-14 18:44:10 -0700

    03/14/2021 06:43 pm

--- caiso_api.py
+++ caiso_api.py
@@ -8,16 +8,13 @@ import datetime
 todays_date = datetime.datetime.now()
 yesterdays_date = todays_date - datetime.timedelta(days = 1)
 tomorrows_date = todays_date + datetime.timedelta(days = 1)
-string_tail = 'T07:00-0000'
 
+string_tail = 'T07:00-0000'
 first_url_string = todays_date.strftime("%Y") + todays_date.strftime("%m") + todays_date.strftime("%d") + string_tail
 second_url_string = tomorrows_date.strftime("%Y") + tomorrows_date.strftime("%m") + tomorrows_date.strftime("%d") + string_tail
 
 # base_url variable to store url
-base_url = "http://oasis.caiso.com/oasisapi/SingleZip?queryname=SLD_FCST&market_run_id=DAM"
-
-# url_tail variable to store the tail of the url
-url_tail = "&version=1"
+base_url = "http://oasis.caiso.com/oasisapi/SingleZip?queryname=PRC_LMP"
 
 # first url argument
 first_url_arg = "&startdatetime=" + first_url_string
@@ -25,11 +22,14 @@ first_url_arg = "&startdatetime=" + first_url_string
 # Second url argument
 second_url_arg = "&enddatetime=" + second_url_string
 
+# url_tail variable to store the tail of the url
+url_tail = "&market_run_id=DAM&version=1&node=STREAMVW_6_N001"
+
 # complete_url variable to store
 # complete url address
 complete_url = base_url + first_url_arg + second_url_arg + url_tail
 
 response = requests.get(complete_url)
 
-print('The response code is:', response.status_code)
+
 
