297705f676bad13a8852ee3fa2c1aa26dbaea485
Juan Carlos Orlando <jcorlandomc@yahoo.com>
2021-03-25 21:21:43 -0700

    03/25/2021 09:21 pm

--- controller.py
+++ controller.py
@@ -7,66 +7,87 @@ from enum import Enum, unique, auto
 pi = pigpio.pi()
 pi.hardware_PWM(12, 8000, 10000)  # 10% duty cycle = 10000 (off)0 to (fully on)1000000
 
-GPIO.setmode(GPIO.BOARD)        # Set up the pins on the board
-GPIO.setwarnings(False)         # using the physical pin layout
+GPIO.setmode(GPIO.BOARD)        # Set up the pins on the board using the physical pin layout
+GPIO.setwarnings(False)
+PIN1 = 29                       # These should remain constant
+PIN2 = 31                       # These should remain constant
+PIN3 = 36                       # These should remain constant
+PIN4 = 37                       # These should remain constant
 GPIO.setup(29,GPIO.OUT)         # Physical pins 29 set as output
 GPIO.setup(31,GPIO.OUT)         # Physical pins 31 set to output
-GPIO.setup(37,GPIO.OUT)         # Physical pins 37 set to output
 GPIO.setup(36,GPIO.OUT)         # Physical pins 36 set to output
+GPIO.setup(37,GPIO.OUT)         # Physical pins 37 set to output
 
-
+@unique
 class State(Enum):
-    initial_state = auto()
+    state_1 = auto()
     state_2 = auto()
     state_3 = auto()
+    state_4 = auto()
+    state_5 = auto()
+    state_6 = auto()
+    state_7 = auto()
 
 class Controller:
     def __init__(self):
-        self.current_state = State.initial_state
+        self.current_state = State.state_1
+        self.switch_states()
     
     def switch_states(self):
-        if self.current_state == State.initial_state:
-            GPIO.output(29,GPIO.HIGH)
-            GPIO.output(31,GPIO.LOW)
-            GPIO.output(37,GPIO.LOW)
-            GPIO.output(36,GPIO.HIGH)
+        if self.current_state == State.state_1:
+            GPIO.output(PIN1,GPIO.LOW)      # OFF
+            GPIO.output(PIN2,GPIO.HIGH)     # ON
+            GPIO.output(PIN3,GPIO.HIGH)     # ON
+            GPIO.output(PIN4,GPIO.LOW)      # OFF
         elif self.current_state == State.state_2:
-            GPIO.output(29,GPIO.LOW)
-            GPIO.output(31,GPIO.HIGH)
-            GPIO.output(37,GPIO.HIGH)
-            GPIO.output(36,GPIO.LOW)
+            GPIO.output(PIN1,GPIO.HIGH)     # ON
+            GPIO.output(PIN2,GPIO.LOW)      # OFF
+            GPIO.output(PIN3,GPIO.LOW)      # OFF
+            GPIO.output(PIN4,GPIO.HIGH)     # ON
         elif self.current_state == State.state_3:
-            GPIO.output(29,GPIO.HIGH)
-            GPIO.output(31,GPIO.LOW)
-            GPIO.output(37,GPIO.HIGH)
-            GPIO.output(36,GPIO.LOW)
+            GPIO.output(PIN1,GPIO.HIGH)     # ON
+            GPIO.output(PIN2,GPIO.HIGH)     # ON
+            GPIO.output(PIN3,GPIO.LOW)      # OFF
+            GPIO.output(PIN4,GPIO.LOW)      # OFF
+        elif self.current_state == State.state_4:
+            GPIO.output(PIN1,GPIO.HIGH)     # ON
+            GPIO.output(PIN2,GPIO.HIGH)     # ON
+            GPIO.output(PIN3,GPIO.HIGH)     # ON
+            GPIO.output(PIN4,GPIO.LOW)      # OFF
+        elif self.current_state == State.state_5:
+            GPIO.output(PIN1,GPIO.LOW)      # OFF
+            GPIO.output(PIN2,GPIO.HIGH)     # ON
+            GPIO.output(PIN3,GPIO.HIGH)     # ON
+        elif self.current_state == State.state_6:
+            GPIO.output(PIN1,GPIO.HIGH)     # ON
+            GPIO.output(PIN2,GPIO.HIGH)     # ON
+            GPIO.output(PIN3,GPIO.HIGH)     # ON
+            GPIO.output(PIN4,GPIO.HIGH)     # ON
+        elif self.current_state == State.state_7:
+            GPIO.output(PIN1,GPIO.HIGH)     # ON
+            GPIO.output(PIN2,GPIO.LOW)      # OFF
+            GPIO.output(PIN3,GPIO.HIGH)     # ON
+            GPIO.output(PIN4,GPIO.HIGH)     # ON
         else:
-            GPIO.output(29,GPIO.HIGH)
-            GPIO.output(31,GPIO.LOW)
-            GPIO.output(37,GPIO.LOW)
-            GPIO.output(36,GPIO.HIGH)
+            GPIO.output(PIN1,GPIO.LOW)      # OFF
+            GPIO.output(PIN2,GPIO.HIGH)     # ON
+            GPIO.output(PIN3,GPIO.HIGH)     # ON
+            GPIO.output(PIN4,GPIO.LOW)      # OFF
     
-    def update_state(self):
-        if self.current_state == State.initial_state:
-            self.current_state = State.state_2
-            self.switch_states()
-        elif self.current_state == State.state_2:
-            self.current_state = State.state_3
-            self.switch_states()
-        elif self.current_state == State.state_3:
-            self.current_state = State.initial_state
-            self.switch_states()
-        else:
-            self.current_state == State.initial_state
+
+    
+    def update_state(self, next_state):
+        self.current_state = next_state
+        self.switch_states()
+
 
 
 controller = Controller()       # Create the controller object from Controller class
 
 while True:
-    controller.update_state()
-    time.sleep(1.5)
-    controller.update_state()
-    time.sleep(1.5)
-    controller.update_state()
-    time.sleep(1.5)
+    for each_state in State:
+        time.sleep(6)
+        controller.update_state(each_state)
+        
+        
 
