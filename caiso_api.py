e410457f7d9fcc175b89e996624521ecb9df3a47
Juan Carlos Orlando <jcorlandomc@yahoo.com>
2021-03-27 02:28:43 -0700

    03/27/2021 02:28 am

--- caiso_api.py
+++ caiso_api.py
@@ -49,21 +49,55 @@ if response.status_code == 200:
         for child2 in child:
             string = child2.tag
             if(string[45:] == "INTERVAL_NUM"):
-                sub_string.append(child2.text)
+                sub_string.append(int(child2.text) - 1)
             elif(string[45:] == "INTERVAL_START_GMT"):
                 sub_string.append(child2.text)
             elif(string[45:] == "INTERVAL_END_GMT"):
                 sub_string.append(child2.text)
             elif(string[45:] == "VALUE"):
-                sub_string.append(child2.text)
+                sub_string.append(float(child2.text))
         values.append(sub_string)
-    
+
     values = sorted(values)
-    for i in range(len(values)):
-        for j in range(len(values[i])):
-            print(values[i][j])
-        print()
-    
+    values.pop(0)
+    values.pop(0)
+
+    #####################################################################################################################################
+    ############################## This portion is to get weather data ##################################################################
+    #####################################################################################################################################
+    file = open('weather_Api_Key.txt', 'r')
+    api_key = file.read()
+    file.close
+
+    # base_url variable to store url
+    base_url = "https://api.openweathermap.org/data/2.5/onecall?lat=32.7760&lon=-177.0713&exclude=current,minutely,alerts,daily&appid="
+
+    # complete_url variable to store
+    # complete url address
+    complete_url = base_url + api_key
+
+
+    # get method of requests module
+    # return response object
+    response = requests.get(complete_url)
+
+
+    # json method of response object
+    # convert json format data into
+    # python format data
+    if response.status_code == 200:
+        x = response.json()     # This is a dictionary
+        x = x['hourly']
+
+    for each in range(0, 48):
+        if(todays_date.day == datetime.datetime.fromtimestamp(x[each]['dt']).day and todays_date.hour == datetime.datetime.fromtimestamp(x[each]['dt']).hour):
+            values[todays_date.hour].append(x[each]['clouds'])
+    #####################################################################################################################################
+    ############################### End of getting weather data #########################################################################
+    #####################################################################################################################################
+    with open('data.json', 'w') as outfile:
+        json.dump(values, outfile)
+
 
 if os.path.exists(filename):        # Delete the file just downloaded
     os.remove(filename)             # Delete the file just downloaded
--- run_caiso_api.sh
+++ run_caiso_api.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/bash
 clear; ./caiso_api.py
 
+
+
+
+
